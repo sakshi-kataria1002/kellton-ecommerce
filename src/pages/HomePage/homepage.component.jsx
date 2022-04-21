413262a56e7cbecfc55f22eacc98a6ef317d3145
Sakshi-Kataria <sakshi.kataria@kelltontech.com>
2022-04-21 12:44:21 +0530

    Homepage Styling

--- src/pages/HomePage/homepage.component.jsx
+++ src/pages/HomePage/homepage.component.jsx
@@ -1,11 +1,48 @@
-import './homepage.style.css'
-import Menu from '../../Menu.component'
+import "./homepage.style.scss"
+
 
 const HomePage = () => {
-    return (
+    return(
         <>
-            <Menu />
-            <h1>Home Page</h1>
+            <div className="homepage" >
+                    <h1>Welcome to my home page</h1>
+                        <div className="directory-menu" >
+                            <div className="menu-item" >
+                                <div className="content" >
+                                    <div className="title" >HATS</div>
+                                    <div className="subtitle" >SHOP NOW</div>
+                                </div>
+                            </div>
+
+                            <div className="menu-item" >
+                                <div className="content" >
+                                    <div className="title" >JACKETS</div>
+                                    <div className="subtitle" >SHOP NOW</div>
+                                </div>
+                            </div>
+
+                            <div className="menu-item" >
+                                <div className="content" >
+                                    <div className="title" >SNEAKERS</div>
+                                    <div className="subtitle" >SHOP NOW</div>
+                                </div>
+                            </div>
+
+                            <div className="menu-item" >
+                                <div className="content" >
+                                    <div className="title" >WOMENS</div>
+                                    <div className="subtitle" >SHOP NOW</div>
+                                </div>
+                            </div>
+
+                            <div className="menu-item" >
+                                <div className="content" >
+                                    <div className="title" >MENS</div>
+                                    <div className="subtitle" >SHOP NOW</div>
+                                </div>
+                            </div>
+                        </div>
+            </div>
         </>
     )
 }
